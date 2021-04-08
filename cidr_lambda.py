23432ae53c580cd712a50e7502b5fb0ecd1173cd
Yauheni <yauheni.maniukevich@gmail.com>
2021-04-08 18:16:31 +0300

    Refactoring

--- cidr_lambda.py
+++ cidr_lambda.py
@@ -1,58 +1,54 @@
 import boto3
-import ipaddress
-from netaddr import IPNetwork
+from netaddr import IPNetwork, cidr_merge, cidr_exclude
 from faker import Faker
 
 
-dynamodbTableName = "vpc_cidr"
+dynamodbTableName = "dev_vpc_cidr"
+dynamodbPrimaryKey = "iad_id"
 dynamodb = boto3.resource('dynamodb')
 
+
+iad_platform = "iad"
+iad_region = "na"
+iad_environment = "dev_iad"
+vpc_masks = 8
+iad_id = "{0}/{1}/{2}".format(iad_platform, iad_region, iad_environment)
+
+# Getting all value from 'vpc_cidr' filed in DynamoDB. 
 def getCidrDDB(dynamodbTableName):
-    # Vpc_cidr is the table name here
     table = dynamodb.Table(dynamodbTableName)
-    # Table scan
     cidrs = table.scan()['Items']
     list_of_cidr = []
     for _ in cidrs: 
-        list_of_cidr.append(_['cidr'])
+        list_of_cidr.append(_['vpc_cidr'])
     return list_of_cidr
 
-def CidrSplitter(network, count):
-    # Finding the minimum power of two 
-    def power_of_two(count):
-        if count > 1:
-            for i in range(1, int(count)):
-                if (2 ** i >= count):
-                    return i
-        else:
-            return 1
-    # Get the main network
-    pair = ipaddress.ip_network(network, strict=False)
-    
-    # The subnet prefix to be added to the original to split the main network into
-    subnet_diff = power_of_two(count)
-
-    # Get subnets of the main network, as a list
-    subnets = list(pair.subnets(prefixlen_diff=subnet_diff))
-    subnet_map = {}
-    for _ in range(0,count):
-        subnet_map[_+1] = str(subnets[_])
-    return {network:subnet_map}
-
-def GeneratorRandomCidr(netmask):
+#Generate random CIDR
+def GeneratorRandomCidr(dynamodbTableName, vpc_masks):
     fake = Faker()
-    return str(IPNetwork(fake.ipv4_private()+'/{0}'.format(netmask)).cidr)
+    vpc_cidr = str(IPNetwork(fake.ipv4_private()+'/{0}'.format(vpc_masks)).cidr)
+    while vpc_cidr in getCidrDDB(dynamodbTableName):
+        vpc_cidr = str(IPNetwork(fake.ipv4_private()+'/{0}'.format(vpc_masks)).cidr)
+    return vpc_cidr
+
+# Check if IP belongs to the network
+def check_vpc(dynamodbTableName, iad_id):
+    try:
+        table = dynamodb.Table(dynamodbTableName)
+        item = table.get_item(Key={'iad_id': iad_id})['Item']
+        return item
+    except Exception as ex:
+        return None
 
 
-def main(dynamodbTableName, netmask, subnet_count):
-    # Generate cidr
-    cidr = GeneratorRandomCidr(netmask)
-
-    # Checking if cidr is not used
-    while cidr in getCidrDDB(dynamodbTableName):
-        cidr = GeneratorRandomCidr(netmask)
-    return CidrSplitter(cidr, subnet_count)
-
-print(main(dynamodbTableName,16,4))
-
-
+##########
+# STEP 1 #
+##########
+# if vpc exist in DynamoDB, otherwise will create
+if check_vpc(dynamodbTableName, iad_id):
+    vpc_network = check_vpc(dynamodbTableName, iad_id)
+    print(vpc_network)
+    #class IPSplitter
+else:
+    vpc_cidr = GeneratorRandomCidr(dynamodbTableName, vpc_masks)
+    
