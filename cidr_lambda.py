c26ce23f0b90d99a0586d80151faa7b224837f1e
Yauheni <yauheni.maniukevich@gmail.com>
2021-04-29 16:39:01 +0300

    Update cidr_lambda.py

--- cidr_lambda.py
+++ cidr_lambda.py
@@ -3,193 +3,190 @@ import logging
 from netaddr import IPNetwork, cidr_merge, cidr_exclude, IPAddress
 import ipaddress
 from faker import Faker
+import re
 import itertools
+import time
 
-dynamodbTableName = "dev_vpc_cidr"
-dynamodb = boto3.resource('dynamodb')
-
-
-iad_platform = "iad"
-iad_region = "na"
-iad_environment = "dev"
-vpc_masks = 8
-log_level = "DEBUG"
+input_public_cidr = ['/31', '/31', '/31']
+input_private_cidr = ['/31','/31','/31']
+input_database_cidr = ['/31','/31','/31']
+input_dmz_cidr = ['/31', '/31','/31','/31']
 
 
-iad_id = "{0}/{1}/{2}".format(iad_platform, iad_region, iad_environment)
-
-def get_logger(name, logging_level):
-    levels = {
-        "NOTSET": logging.NOTSET,
-        "DEBUG": logging.DEBUG,
-        "INFO": logging.INFO,
-        "WARNING": logging.WARNING,
-        "ERROR": logging.ERROR,
-        "CRITICAL": logging.CRITICAL,
-    }
-
-    logger = logging.getLogger(name=name)
-    logger.setLevel(levels[logging_level])
-
-    return logger
-
-log = get_logger("cidr_managment", log_level)
-
-#############################################################
+#Find Suit subnet in order to split
 def closest(lst, prefix):
-        suitable_prefix = next((i for i in lst if prefix >= i), None)
-        if suitable_prefix == None:
-            raise Exception("Cannot find free network for prefix: {0}".format(prefix))
-        return suitable_prefix
+    suitable_prefix = next((i for i in lst if prefix >= i), None)
+    if suitable_prefix == None:
+        raise Exception(
+            "Cannot find free network for prefix: {0}".format(prefix))
+    return suitable_prefix
 
-# Getting all value from 'vpc_cidr' filed in DynamoDB. 
-def getCidrDDB(dynamodbTableName):
-    table = dynamodb.Table(dynamodbTableName)
-    cidrs = table.scan()['Items']
-    list_of_cidr = []
-    for _ in cidrs: 
-        list_of_cidr.append(_['vpc_cidr'])
-    return list_of_cidr
-
-#Generate random CIDR
-def GeneratorRandomCidr(dynamodbTableName, vpc_masks):
+# Generate random CIDR
+def GeneratorRandomCidr(VpcMask, AllExistsVpc = []):
     fake = Faker()
-    vpc_cidr = str(IPNetwork(fake.ipv4_private()+'/{0}'.format(vpc_masks)).cidr)
-    while vpc_cidr in getCidrDDB(dynamodbTableName):
-        vpc_cidr = str(IPNetwork(fake.ipv4_private()+'/{0}'.format(vpc_masks)).cidr)
+    vpc_cidr = str(IPNetwork(fake.ipv4_private() +
+                             '/{0}'.format(VpcMask)).cidr)
+    while vpc_cidr in AllExistsVpc:
+        vpc_cidr = str(IPNetwork(fake.ipv4_private() +
+                                 '/{0}'.format(VpcMask)).cidr)
     return vpc_cidr
 
-# Check if IP belongs to the network
-def vpc_exists(dynamodbTableName, iad_id):
-    try:
-        table = dynamodb.Table(dynamodbTableName)
-        item = table.get_item(Key={'iad_id': iad_id})['Item']
-        return item
-    except Exception as ex:
-        return None
-
+def discover_new_subnets(used_cidrs, desired_public_cidr=[]):
+    # The function compares used and desired prefix, if there are changes, the whole sequence will be removed.
+    # 1. Used: ['/31', '/31', '/31'], desired = ['/31', '/31', '/31'] => No changes
+    # 2. Used: ['/31', '/31', '/31'], desired = ['/31', '/32', '/31'] => New suqence is ['/32', '/31'] and only first subnet with no changes
+    used_prefix = [re.search('\/(.*)', i).group(0) for i in used_cidrs]
+    new_prefix = []
+    count = -1
+    for desired, used in itertools.zip_longest(desired_public_cidr, used_prefix):
+        count += 1
+        if desired != used and used != None:
+            del used_cidrs[count:]
+            new_prefix = desired_public_cidr[count:]
+            break
+        elif len(desired_public_cidr) > len(used_prefix):
+            new_prefix = desired_public_cidr[len(used_prefix):]
+            break
+    new_prefix = [_[1:] for _ in new_prefix]
+    print
+    return used_cidrs, new_prefix
 
 class IPSplitter():
-    def __init__(self, vpc_cidr, used_subnets = []):
+    def __init__(self, vpc_cidr, used_subnets=[]):
         self.availible_subnets = set((IPNetwork(vpc_cidr),))
-        self.used_subnets = cidr_merge(used_subnets) 
-    def get_free_subnets(self):
-    # Calculate free subnets in existing VPC. 
-    # Return is a list: [IPNetwork('192.168.0.0/30'), IPNetwork('192.168.0.16/28')]
-        for ip_network in self.availible_subnets:
-           self.availible_subnets = list(cidr_exclude(ip_network, self.used_subnets[0]))
-        for used in self.used_subnets: 
-            for free in list(self.availible_subnets):
-                if IPNetwork(used) in IPNetwork(free):
-                    self.availible_subnets.remove(free)
-                    self.availible_subnets = self.availible_subnets + cidr_exclude(free,used)
+        self.used_subnets = cidr_merge(used_subnets)
+
+    # Calculate free subnets in existing VPC.
+    def GetFreeSubnets(self):
+        # Return is a list: [IPNetwork('192.168.0.0/30'), IPNetwork('192.168.0.16/28')]
+        if len(self.used_subnets) != 0:
+            for ip_network in self.availible_subnets:
+                self.availible_subnets = list(
+                    cidr_exclude(ip_network, self.used_subnets[0]))
+            for used in self.used_subnets:
+                for free in list(self.availible_subnets):
+                    if IPNetwork(used) in IPNetwork(free):
+                        self.availible_subnets.remove(free)
+                        self.availible_subnets = self.availible_subnets + \
+                            cidr_exclude(free, used)
+        else:
+            self.availible_subnets = list(self.availible_subnets)
         return self.availible_subnets
 
-    def get_subnet(self, prefix):
-        self.get_free_subnets()
     # Finding suitable subnets (by the closest prefix), splitting getting subnets
+    def GetSubnet(self, prefix):
+        self.get_free_subnets()
     # Return all prefix of free subnet: [30, 29, 28]
-        free_subnets_prefix = [ _.prefixlen for _ in self.availible_subnets ]
-    ## Return max prefix of suit subnets: 28
+        free_subnets_prefix = [_.prefixlen for _ in self.availible_subnets]
+    # Return max prefix of suit subnets: 28
         max_suit_prefix = closest(free_subnets_prefix, prefix)
-    ## Return suit free subnet: 192.168.0.16/28
-        cidr = self.availible_subnets[free_subnets_prefix.index(max_suit_prefix)]
-    ## Remove suit free cidr
+    # Return suit free subnet: 192.168.0.16/28
+        cidr = self.availible_subnets[free_subnets_prefix.index(
+            max_suit_prefix)]
+    # Remove suit free cidr
         self.availible_subnets.remove(cidr)
-    ## Getting suit subnets 
-        subnet = list(cidr.subnet(prefix, count = 1))
-    ## Exlude subnet, and add free subnets to list "self.availible_subnets"
-        self.availible_subnets = self.availible_subnets + cidr_exclude(cidr,subnet[0])
-        return subnet
+    # Getting suit subnets
+        subnet = list(cidr.subnet(prefix, count=1))
+    # Exlude subnet, and add free subnets to list "self.availible_subnets"
+        self.availible_subnets = self.availible_subnets + \
+            cidr_exclude(cidr, subnet[0])
+        return str(subnet[0])
 
-#vpc1 = IPSplitter(vpc_cidr,active_subnets)
-#vpc1.get_free_subnets()
-#print(vpc1.get_subnet(30))
-#print(vpc1.get_subnet(30))
+class DynamoDB():
+    def __init__(self, DynamoTableName, iad_id):
+        dynamodb = boto3.resource('dynamodb')
+        self.table = dynamodb.Table(DynamoTableName)
+        self.dynamodb_table_name = DynamoTableName
+        self.iad_id = iad_id
+    # Checking if an VPC exists in the DynamoDB by IAD tag
+    def VpcExists(self):
+        try:
+            item = self.table.get_item(Key={'iad_id': self.iad_id})['Item']
+            return item
+        except Exception as ex:
+            return None
 
-input_private_cidrs = ["/31","/31","/31"]
-input_public_cidr = ["/31","/31", "/32"]
+    # Gathering all vpc in DynamoDB
+    def GetAllVpc(self):
+        items = self.table.scan()['Items']
+        all_vpc = [vpc['vpc_cidr'] for vpc in items]
+        return all_vpc
 
-public_cidrs = ['192.168.0.0/31', '192.168.0.2/31']
-private_cidr = ['192.168.0.4/31', '192.168.0.6/31', '192.168.0.8/31']
-def discover_new_subnets(used_subnet, desired_subnet):
-    print()
+    # Updating VPC information
+    def UpdateItemsDDB(self, subnet_name, lst_subnet):
+        response = self.table.update_item(
+            Key={
+                'iad_id': self.iad_id,
+            },
+            UpdateExpression='SET {0} = :val1'.format(subnet_name),
+            ExpressionAttributeValues={
+                ':val1': lst_subnet
+            }
+        )
+        return response
+    # Creating Item in DynamoDB
+    def CreateItemsDDB(self, new_item_content):
+        response = self.table.put_item(
+            Item = new_item_content
+    )           
 
 ##########
-# STEP 1 #
+## Main ##
 ##########
 # if vpc exist in DynamoDB, otherwise will create
-if vpc_exists(dynamodbTableName, iad_id):
-    vpc_network = vpc_exists(dynamodbTableName, iad_id)
+iad_platform = "iad"
+iad_region = "na"
+iad_environment = ""
+vpc_masks = 8
+iad_id = "{0}/{1}/{2}".format(iad_platform, iad_region, iad_environment)
+
+dynamodbTableName = "dev_vpc_cidr"
+db = DynamoDB(dynamodbTableName, iad_id)
+if db.VpcExists():
+    vpc_network = db.VpcExists()
     vpc_cidr = vpc_network['vpc_cidr']
     database_cidrs = vpc_network.get('database_subnets', list())
     private_cidrs = vpc_network.get('private_subnets', list())
     public_cidrs = vpc_network.get('public_subnets', list())
     dmz_cidrs = vpc_network.get('dmz_subnets', list())
     used_subnets = database_cidrs + private_cidrs + public_cidrs + dmz_cidrs
-
 # Find diff between existing subnet and new
-    print(private_cidrs)
-    print(public_cidrs)
-#    vpc = IPSplitter(vpc_cidr, used_subnets)
-
+    public_cidrs, new_public_mask = discover_new_subnets(
+        public_cidrs, input_public_cidr)
+    private_cidrs, new_private_mask = discover_new_subnets(
+        private_cidrs, input_private_cidr)
+    database_cidrs, new_database_mask = discover_new_subnets(
+        database_cidrs, input_database_cidr)
+    dmz_cidrs, new_dmz_mask = discover_new_subnets(dmz_cidrs, input_dmz_cidr)
+# Getting new subnets cidr, and plus with old items
+    vpc = IPSplitter(vpc_cidr, used_subnets)
+    public_cidrs = public_cidrs + \
+        [vpc.GetSubnet(int(_)) for _ in new_public_mask]
+    private_cidrs = private_cidrs + \
+        [vpc.GetSubnet(int(_)) for _ in new_private_mask]
+    database_cidrs = database_cidrs + \
+        [vpc.GetSubnet(int(_)) for _ in new_database_mask]
+    dmz_cidrs = dmz_cidrs + [vpc.GetSubnet(int(_)) for _ in new_dmz_mask]
+# Update DunamoDB table
+    db.UpdateItemsDDB('public_subnets', public_cidrs)
+    db.UpdateItemsDDB('private_subnets', private_cidrs)
+    db.UpdateItemsDDB('database_subnets', database_cidrs)
+    db.UpdateItemsDDB('dmz_subnets', dmz_cidrs)
 else:
-    vpc_cidr = GeneratorRandomCidr(dynamodbTableName, vpc_masks)
-
-#class IPSplitter():
-#    def __init__(self, vpc_cidr):
-#        self.avail_ranges = set((IPNetwork(vpc_cidr),))
-#
-#    def get_subnet(self, prefix, count=2):
-#        for ip_network in self.avail_ranges:
-#            subnets = list(ip_network.subnet(prefix, count=count))
-#            if not subnets:
-#                continue
-#            self.remove_avail_range(ip_network)
-#            self.avail_ranges = self.avail_ranges.union(set(cidr_exclude(ip_network, cidr_merge(subnets)[0])))
-#            return subnets
-#
-#    def get_available_ranges(self):
-#        return self.avail_ranges
-#
-#    def remove_avail_range(self, ip_network):
-#        self.avail_ranges.remove(ip_network)
-
-
-
-#def put_item(dynamodbTableName):
-#    dynamodb = boto3.resource('dynamodb')
-#    table = dynamodb.Table(dynamodbTableName)
-#    item = table.put_item(
-#        Item={
-#            'cidr': "192.168.0.0/16",
-#            'private_subnet': {
-#                '192.168.0.0/19',
-#                '192.168.32.0/19'
-#            },
-#            'public_subnet': {
-#                '192.168.128.0/19',
-#                '192.168.160.0/19'
-#            },
-#
-#        }
-#    )
-#    return item
-
-
-#print(vpc.get_available_ranges())
-
-
-##a = cidr_exclude(networks, IPNetwork(
-#print(list(networks.subnet(30,2)))
-#
-#for i in networks:
-#    print("availible: {0}".format(i))
-#
-#def get_item(dynamodbTableName):
-#    dynamodb = boto3.resource('dynamodb')
-#    table = dynamodb.Table(dynamodbTableName)
-#    item = table.get_item(Key={'cidr':'192.168.0.0/16'})['Item']
-#    return item
-#
-
+    vpc_cidr = GeneratorRandomCidr(dynamodbTableName, vpc_masks, db.GetAllVpc())
+    vpc = IPSplitter(vpc_cidr)
+    public_cidrs = [ vpc.GetSubnet(int(_[1:])) for _ in input_public_cidr]
+    private_cidrs = [ vpc.GetSubnet(int(_[1:])) for _ in input_private_cidr]
+    database_cidrs = [ vpc.GetSubnet(int(_[1:])) for _ in input_database_cidr]
+    dmz_cidrs = [ vpc.GetSubnet(int(_[1:])) for _ in input_dmz_cidr]
+    
+    content_of_item = {
+        'iad_id': iad_id,
+        'vpc_cidr': vpc_cidr,
+        'public_subnets': public_cidrs,
+        'private_subnets': private_cidrs,
+        'database_subnets': database_cidrs,
+        'dmz_subnets': dmz_cidrs
+    }
+    
+    db.CreateItemsDDB(content_of_item)
